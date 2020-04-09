9c9f534e965f3ea6593835a4b1b510c94108186c
Mirco De Zorzi <mircodezorzi@protonmail.com>
2020-04-09 18:10:15 +0200

    feat(clangformat): add fallback
    * the plugin will fallback to using only `highlight` if clang-format
    isn't present on the system

--- lib/jekyll/clangformat.rb
+++ lib/jekyll/clangformat.rb
@@ -4,13 +4,18 @@ module Jekyll
   class ClangFormat < Liquid::Tag
 
     def initialize(tag_name, markup, tokens)
+      @has_clangformat = `which clang-format > /dev/null 2>&1`
       @input = markup.split(' ')[0]
       super
     end
 
     def render(context)
-      output = `clang-format <<< "#{context[@input]}"`.strip
-      Liquid::Template.parse("{% highlight c++ %}#{output}{% endhighlight %}").render(context)
+      if @has_clangformat
+        output = `clang-format <<< "#{context[@input]}"`
+        Liquid::Template.parse("{% highlight c++ %}#{output}{% endhighlight %}").render(context)
+      else
+        Liquid::Template.parse("{% highlight c++ %}#{@input}{% endhighlight %}").render(context)
+      end
     end
 
   end
